a9b7aeb01e45f9482f583edeee67033dca171ed5
AdarshIntellibottech <90606646+AdarshIntellibottech@users.noreply.github.com>
2022-09-08 23:55:29 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const vandobj = {
-  name: "Adarsh",
-  usn: "4jn17cs120",
-  degree: "CS"
+//use of spread operator
+
+let student = {
+  name: "Adarsh Naik",
+  age: 19,
+  address : "Shimoga"
 }
 
-vandobj.name="Santosh"
+let student1 = {...student, age:23, name:"Bharath"}
 
-console.log(vandobj)
+console.log(student1)
+
+// rest operator use in objects
+let employee = {
+  ename: "atal",
+  tech: "react",
+  state: "Bihar"
+}
+const {tech, ...baki}= employee;
+console.log(tech)
