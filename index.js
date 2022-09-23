030eb7ce4ed6842b83a335f537558a0d2749a6cf
AdarshIntellibottech <90606646+AdarshIntellibottech@users.noreply.github.com>
2022-09-23 16:46:20 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-let arr1 = [1, 2, 3, 4, 5];
-
-let arr2 = [...arr1];
-
-arr2[3] = 45;
-console.log(arr2);
-console.log(arr1); //deep copy where references of both arr2 and arr1 is different
+let value = "98:23"
+let arr1 = [];
+arr1=value.split("")
+console.log(arr1)
